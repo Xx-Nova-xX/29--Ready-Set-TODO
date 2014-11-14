67741084d409342e688be55522b7562300f1efe6
Ally Hinton <ally@socialesteemmedia.com>
2014-11-14 15:31:48 -0500

    Close to completing the correct bold function

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 (function( window ) {
 	'use strict';
 
-$("#filters").click(function() { /*all function*/
+$("#filters").click(function() { /*selected = All function*/
 	$('.active, .completed').show();
+	$('.selected').css("font-weight", "bold");
+	$('.active, .completed').css("font-weight", "none");
 });
 
 $('#filters .active').click(function() { /*active function*/
 	$('.active').show();
+	$('.active').css("font-weight", "bold");
+	$('.completed, .selected').css("font-weight", "none");
 	$('#todo-list .completed').hide();
 	return false;
 });
 
 $('#filters .completed').click(function() { /*completed function */
 	$('.completed').show();
+	$('.completed').css("font-weight", "bold");
+	$('.active, .selected').css("font-weight", "none");
 	$('#todo-list .active').hide();
 	return false;
 });
